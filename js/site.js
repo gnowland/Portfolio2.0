01f51786fc2e199f6a5527d49e009b0e28590b40
Gifford Nowland <gnowland@gmail.com>
2014-03-26 08:16:25 -0500

    working on menu

--- js/site.js
+++ js/site.js
@@ -1,4 +1,4 @@
-//Set header.push heght
+//Set body margin-top heght to bump down page height of header
 $( 'body' ).css( "margin-top", $('header').outerHeight());
 
 // Navigation Menu
@@ -6,34 +6,34 @@ $(function() { //When the document loads
 
   var sections = $('section');
   var navigation_links = $('nav a');
-  var navoffset = $('header').outerHeight();
+  var headerheight = $('header').outerHeight();
+
+  navigation_links.on('click', function (e) {
+    e.preventDefault();
+
+    var url = $(this).attr('href'); //must be inside this .on('click') event
+
+    $('html, body').animate({
+        scrollTop:  $('html').find($(this).attr('href')).offset().top-headerheight
+        }, 400, 'swing', function () {
+
+          window.location.hash = url ;
+
+        });
+  });
 
   sections.waypoint({
-    handler: function(event, direction) {
+    handler: function(direction) {
 
       var active_section;
       active_section = $(this);
-      if (direction === "up") active_section = active_section.prev();
+      if (direction === "up") { active_section = active_section.prev(); }
 
       var active_link = $('nav a[href="#' + active_section.attr("id") + '"]');
       navigation_links.removeClass("selected");
       active_link.addClass("selected");
 
     },
-    offset: navoffset-1
-  })
-
-  navigation_links.on('click', function (e) {
-    e.preventDefault();
-
-    var locale= $(this).attr('href');
-
-    $('html, body').animate({
-        scrollTop:  $('html').find($(this).attr('href')).offset().top-navoffset+1
-        }, 1200, 'swing', function () {
-
-          window.location.hash = locale ;
-
-        });
+    offset: headerheight
   });
 });
