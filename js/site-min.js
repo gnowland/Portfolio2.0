fdcffc49ce99f221c7a3f01bc4f26e770fbc9c08
Gifford Nowland <gnowland@gmail.com>
2014-03-26 10:34:13 -0500

    finally got waypoints under control

--- js/site-min.js
+++ js/site-min.js
@@ -525,7 +525,7 @@ https://github.com/imakewebthings/jquery-waypoints/blob/master/licenses.txt
 ********************************************** */
 
 //Set body margin-top heght to bump down page height of header
-$( 'body' ).css( "margin-top", $('header').outerHeight());
+$( 'body' ).css( 'margin-top', $('header').outerHeight());
 
 // Navigation Menu
 $(function() { //When the document loads
@@ -540,7 +540,7 @@ $(function() { //When the document loads
     var url = $(this).attr('href'); //must be inside this .on('click') event
 
     $('html, body').animate({
-        scrollTop:  $('html').find($(this).attr('href')).offset().top-headerheight
+        scrollTop:  $('html').find($(this).attr('href')).offset().top-headerheight+1
         }, 400, 'swing', function () {
 
           window.location.hash = url ;
@@ -556,10 +556,28 @@ $(function() { //When the document loads
       if (direction === "up") { active_section = active_section.prev(); }
 
       var active_link = $('nav a[href="#' + active_section.attr("id") + '"]');
-      navigation_links.removeClass("selected");
-      active_link.addClass("selected");
+      navigation_links.removeClass('selected');
+      active_link.addClass('selected');
 
     },
     offset: headerheight
   });
+
+// Check if at bottom of page, if so, add class to last <a> as sometimes the last div
+// isnt long enough to scroll to the top of the page and trigger the active state.
+
+  $('section:last').waypoint({
+    handler: function(direction){
+      if (direction === 'down') {
+          navigation_links.removeClass('selected');
+          navigation_links.not( ".external" ).last().addClass('selected');
+      }
+      else {
+          navigation_links.removeClass('selected');
+          navigation_links.not( ".external" ).eq(-2).addClass('selected');
+      }
+    },
+    offset: 'bottom-in-view'
+  });
+
 });
