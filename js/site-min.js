890bd7b70e0e59f2da0c73124b65fd4558f2bee6
Gifford Nowland <gnowland@gmail.com>
2014-03-27 06:00:51 -0500

    got all waypoints working

--- js/site-min.js
+++ js/site-min.js
@@ -524,6 +524,10 @@ https://github.com/imakewebthings/jquery-waypoints/blob/master/licenses.txt
      Begin site.js
 ********************************************** */
 
+// REMOVE BEFORE SHIP!!! :
+/*jshint devel:true */
+// console.log("You are here:", this.id);
+
 //Set body margin-top heght to bump down page height of header
 $( 'body' ).css( 'margin-top', $('header').outerHeight());
 
@@ -531,22 +535,24 @@ $( 'body' ).css( 'margin-top', $('header').outerHeight());
 $(function() { //When the document loads
 
   var sections = $('section');
-  var navigation_links = $('nav a');
+  var nav_a = $('nav a');
   var headerheight = $('header').outerHeight();
 
-  navigation_links.on('click', function (e) {
-    e.preventDefault();
-
-    var url = $(this).attr('href'); //must be inside this .on('click') event
-
-    $('html, body').animate({
-        scrollTop:  $('html').find($(this).attr('href')).offset().top-headerheight+1
-        }, 400, 'swing', function () {
-
-          window.location.hash = url ;
-
-        });
+// remove ".external" from firing
+  nav_a.parent().not('.external').on('click', function (event) {
+      // Don't reload the page
+      event.preventDefault();
+      // Find the url
+      var url = $(this).children('a').attr('href');
+      // go to the section
+      $('html, body').animate({
+          scrollTop:  $('html').find($(this).children('a').attr('href')).offset().top-headerheight+1
+          }, 400, 'swing', function () {
+            window.location.hash = url ;
+          });
   });
+
+  // Highlighting
 
   sections.waypoint({
     handler: function(direction) {
@@ -556,8 +562,8 @@ $(function() { //When the document loads
       if (direction === "up") { active_section = active_section.prev(); }
 
       var active_link = $('nav a[href="#' + active_section.attr("id") + '"]');
-      navigation_links.removeClass('selected');
-      active_link.addClass('selected');
+      nav_a.parent().removeClass('selected');
+      active_link.parent().addClass('selected');
 
     },
     offset: headerheight
@@ -565,19 +571,32 @@ $(function() { //When the document loads
 
 // Check if at bottom of page, if so, add class to last <a> as sometimes the last div
 // isnt long enough to scroll to the top of the page and trigger the active state.
-
   $('section:last').waypoint({
     handler: function(direction){
       if (direction === 'down') {
-          navigation_links.removeClass('selected');
-          navigation_links.not( ".external" ).last().addClass('selected');
+          nav_a.parent().removeClass('selected');
+          nav_a.parent().not('.external').last().addClass('selected');
       }
+      // Add class to the section above the last div once the user starts scrolling up again,
+      // then the normal "highlighting" function takes back over control
       else {
-          navigation_links.removeClass('selected');
-          navigation_links.not( ".external" ).eq(-2).addClass('selected');
+          nav_a.parent().removeClass('selected');
+          nav_a.parent().not('.external').eq(-2).addClass('selected');
       }
     },
     offset: 'bottom-in-view'
+  });
+
+  // Check if scrolled to top of page, if so, add class to first <a>
+  // (forcefix for when all content fits on one screen)
+  $('section:first').waypoint({
+    handler: function(direction){
+      if (direction === 'up') {
+        nav_a.parent().removeClass('selected');
+        nav_a.parent().not('.external').first().addClass('selected');
+      }
+    },
+    offset: headerheight-1
   });
 
 });
